486786e971663c5a9623b336c3765850fa34ed92
Mike Schutte <tmikeschutte@gmail.com>
2018-07-17 10:38:16 -0400

    Add base index and spec

---
+++ index.js
@@ -0,0 +1,7 @@
+const booksInYear = (books, year) => {
+};
+
+
+module.exports = {
+  booksInYear,
+}
---
+++ spec.js
@@ -0,0 +1,19 @@
+const test = require("tape")
+const { booksInYear } = require("./index")
+
+const books = [
+  { title: "To Kill A Mockingbird", author: "Harper Lee", year: 1960 },
+  { title: "The Secret History", author: "Donna Tartt", year: 1992 },
+  { title: "Infinite Jest", author: "David Foster Wallace", year: 1996 },
+  { title: "Fight Club", author: "Chuck Palahniuk", year: 1996 }
+]
+
+test("booksInYear filters an array of books by year", t => {
+  t.plan(1)
+  const actual = booksInYear(books, 1996)
+  const expected = [
+    { title: "Infinite Jest", author: "David Foster Wallace", year: 1996 },
+    { title: "Fight Club", author: "Chuck Palahniuk", year: 1996 }
+  ]
+  t.same(actual, expected)
+})
