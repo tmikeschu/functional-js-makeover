4d3d3d496ed59afa3d68326a224d94fd10f546df
Mike Schutte <tmikeschutte@gmail.com>
2018-07-23 13:09:37 -0400

    Add hp to books

--- spec.js
+++ spec.js
@@ -2,10 +2,31 @@ const test = require("tape")
 const { booksInYear } = require("./index")
 
 const books = [
-  { title: "To Kill A Mockingbird", author: "Harper Lee", year: 1960 },
-  { title: "The Secret History", author: "Donna Tartt", year: 1992 },
-  { title: "Infinite Jest", author: "David Foster Wallace", year: 1996 },
-  { title: "Fight Club", author: "Chuck Palahniuk", year: 1996 },
+  {
+    title: "To Kill A Mockingbird",
+    author: "Harper Lee",
+    year: 1960,
+  },
+  {
+    title: "The Secret History",
+    author: "Donna Tartt",
+    year: 1992,
+  },
+  {
+    title: "Infinite Jest",
+    author: "David Foster Wallace",
+    year: 1996,
+  },
+  {
+    title: "Fight Club",
+    author: "Chuck Palahniuk",
+    year: 1996,
+  },
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    year: 1997,
+  },
 ]
 
 test("booksInYear filters an array of books by year", t => {
