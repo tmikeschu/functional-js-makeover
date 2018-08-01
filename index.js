789ca9a6301b535575e1ec44f21acc218d136770
Mike Schutte <tmikeschutte@gmail.com>
2018-07-31 21:29:06 -0400

    Create wrapper functions

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const whereYear = year => books => books.filter(book => book.year === year)
+const filter = predicate => xs => xs.filter(predicate)
+const equals = a => b => a === b
+const prop = propName => obj => obj[propName]
+
+const whereYear = year => filter(item => equals(year)(prop("year")(item)))
 
 module.exports = {
   whereYear,
